afb1aec356cd854e71d1b4733e183d613c67d5dc
ankit <ankitjha0723@gmail.com>
2025-08-18 23:25:22 +0530

    User Scema

---
+++ Backend/models/user.model.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const userSchema = new mongoose.Schema({
+  fullname: {
+    firstname: {
+      type: String,
+      required: true,
+      minlength: [3, "First name must be at least 3 Character log"],
+    },
+    lastname: {
+      type: String,
+      minlength: [3, "lastname name must be at least 3 Character log"],
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    socketId: {
+      type: String,
+    },
+  },
+});
+
+userSchema.methods.genrateAuthToken = function () {
+  const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET);
+  return token;
+};
+
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.static.hashPassword = async function (password) {
+  return await bcrypt.hash(password, 10);
+};
+
+const userModel = mongoose.model("User", userSchema);
+
+module.exports = userModel;
